816ffaf639d65fcfabfe04390e18971705d8503f
Adam S Lucas <adamlucascoding@gmail.com>
2022-09-02 21:45:03 -0600

    first commit

---
+++ fizzbuzz/script.js
@@ -0,0 +1,33 @@
+// let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz to: "));
+
+// for (let i = 1; i <= answer; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//         console.log("Fizz");
+//     } else if (i % 5 === 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(i);
+//     }
+
+// }
+
+// const a = 5;
+// const b = 10;
+
+// function add() {
+//     return c;
+// }
+
+// function print() {
+//     add();
+// }
+
+// print();
+
+const str1 = "hello ";
+const str2 = "world!";
+
+const message = str1.concat(str2);
+console.log(message);
